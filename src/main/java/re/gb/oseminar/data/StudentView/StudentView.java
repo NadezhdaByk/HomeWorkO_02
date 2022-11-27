90291c8775b98ad1edc534277088dca7172f5e04
Nadezhda <nbykova79@gmail.com>
2022-11-27 10:59:30 +0300

    View for Student

---
+++ src/main/java/re/gb/oseminar/data/StudentView/StudentView.java
@@ -0,0 +1,17 @@
+package re.gb.oseminar.data.StudentView;
+
+import re.gb.oseminar.data.data.User;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+public class StudentView {
+
+    public void sendOnConsole(List<User> users){
+        Logger logger = Logger.getAnonymousLogger();
+
+        for (User item: users){
+            logger.info(users.toString());
+        }
+    }
+}
